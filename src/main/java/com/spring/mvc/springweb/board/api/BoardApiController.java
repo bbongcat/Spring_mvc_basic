39eab7f6c10b859a851dafc27fe3a055b59ed5e2
yj <lollolllo328@gmail.com>
2021-04-14 19:57:49 +0900

    [21.04.14] # Board API 학습

--- src/main/java/com/spring/mvc/springweb/board/api/BoardApiController.java
+++ src/main/java/com/spring/mvc/springweb/board/api/BoardApiController.java
@@ -4,6 +4,7 @@ import com.spring.mvc.springweb.board.domain.Board;
 import com.spring.mvc.springweb.board.mapper.BoardMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -14,18 +15,37 @@ public class BoardApiController {
 
     private final BoardMapper boardMapper;
 
+    //게시물 목록 페이지 열기
+    @GetMapping("/list")
+    public ModelAndView listPage() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("board-api/list");
+        return mv;
+    }
+
+    //게시물 상세보기 페이지 열기
+    @GetMapping("/detail")
+    public ModelAndView contentPage(int boardNo) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("board-api/content");
+        mv.addObject("bno", boardNo);
+        return mv;
+    }
+
     //게시물 전체 조회
     @GetMapping("/")
     public List<Board> list() {
         System.out.println("/api/board/ GET!");
         return boardMapper.getArticles();
     }
+
     //게시물 개별 조회
     @GetMapping("/{boardNo}")
     public Board findOne(@PathVariable int boardNo) {
-        System.out.println("/api/board/"+ boardNo +" GET!");
+        System.out.println("/api/board/" + boardNo + " GET!");
         return boardMapper.getContent(boardNo);
     }
+
     //게시물 등록
     @PostMapping("/")
     public String register(@RequestBody Board board) {
@@ -33,6 +53,7 @@ public class BoardApiController {
         boardMapper.insertArticle(board);
         return "regSuccess";
     }
+
     //게시물 수정
     @PutMapping("/{boardNo}")
     public String update(@PathVariable int boardNo, @RequestBody Board board) {
@@ -44,7 +65,7 @@ public class BoardApiController {
     //게시물 삭제
     @DeleteMapping("/{boardNo}")
     public String delete(@PathVariable int boardNo) {
-        System.out.println("/api/board/"+ boardNo +" DELETE!");
+        System.out.println("/api/board/" + boardNo + " DELETE!");
         boardMapper.deleteArticle(boardNo);
         return "delSuccess";
     }
